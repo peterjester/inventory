204d05933ca3eb3c16cf5deee72cfdd66121ab56
Peter <peter.j.jester@gmail.com>
2018-11-12 20:05:45 -0500

    added camera functionality

--- app/src/main/java/com/example/peterjester/inventory/activity/ItemInfo.java
+++ app/src/main/java/com/example/peterjester/inventory/activity/ItemInfo.java
@@ -1,17 +1,53 @@
 package com.example.peterjester.inventory.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.peterjester.inventory.R;
 
-public class ItemInfo extends AppCompatActivity {
+public class ItemInfo extends AppCompatActivity implements View.OnClickListener {
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
+    private EditText itemView = null;
+    private EditText descriptionView = null;
+    private EditText locationView = null;
+    private EditText beaconView = null;
+    private ImageView imageView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_info);
+
+        itemView = findViewById(R.id.itemEditView);
+        descriptionView = findViewById(R.id.descriptionEditView);
+        locationView = findViewById(R.id.locationEditView);
+        beaconView = findViewById(R.id.beaconEditView);
+
+        imageView = findViewById(R.id.imageView);
+        imageView.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.imageView: dispatchTakePictureIntent(); break;
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+
 }
