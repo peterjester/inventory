5118f7d5d3c336053b55af8fdf35112d1c00c818
Peter <peter.j.jester@gmail.com>
2018-11-22 10:29:14 -0500

    added in listener for searchView

--- app/src/main/java/com/example/peterjester/inventory/activity/SearchItemActivity.java
+++ app/src/main/java/com/example/peterjester/inventory/activity/SearchItemActivity.java
@@ -1,35 +1,44 @@
 package com.example.peterjester.inventory.activity;
 
-import android.app.SearchManager;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 import com.example.peterjester.inventory.R;
 
 public class SearchItemActivity extends AppCompatActivity {
 
     private SearchView searchView = null;
-
+    private TextView resultsText = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_items);
 
         searchView = findViewById(R.id.searchView);
+        resultsText = findViewById(R.id.resultsText);
 
-        // Get the intent, verify the action and get the query
-        Intent intent = getIntent();
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            doMySearch(query);
-        }
+        setupListeners();
 
     }
 
-    private void doMySearch(String query) {
-        Log.i("Trying this out", "doMySearch: " + query);
+    private void setupListeners() {
+        // perform set on query text listener event
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // do something on text submit
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // @todo add in adapter, and filter data based on text input
+                resultsText.setText(newText);
+                return false;
+            }
+        });
     }
 }
