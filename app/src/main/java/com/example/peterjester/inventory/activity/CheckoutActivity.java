ee4a9effb225ba8bdccf83a2009fa9e77ba097b6
CraigRoland <samanthalevan@Samanthas-MacBook-Pro.local>
2018-12-03 18:24:52 -0500

    Initial start for getting google map services to get current location

--- app/src/main/java/com/example/peterjester/inventory/activity/CheckoutActivity.java
+++ app/src/main/java/com/example/peterjester/inventory/activity/CheckoutActivity.java
@@ -1,16 +1,68 @@
 package com.example.peterjester.inventory.activity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.example.peterjester.inventory.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class CheckoutActivity extends AppCompatActivity {
 
+    private FusedLocationProviderClient mFusedLocationClient;
+
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.e("String", "calling the get last location");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
 
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.e("String", "calling the get last location 1");
+//            ActivityCompat.requestPermissions(this,
+//                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},
+//                    123);
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            Log.e("String", "calling the get last location 1");
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                Log.e("String", "calling the get last location 2");
+                                // Logic to handle location object
+
+                            }
+                        }
+                    }) .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.e("MapDemoActivity", "Error trying to get last GPS location");
+                    e.printStackTrace();
+                }
+            });
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+            return;
+        }
+
+
     }
+
 }
